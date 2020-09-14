b9caeb2e0507132b0d458162418ecc668eb8acff
Frank Marazita <fmarazita1@gmail.com>
2020-09-15 05:58:02 +1000

    Now using uwp lines and sliders for values

--- Gamebar Crosshair/Crosshair.xaml.cs
+++ Gamebar Crosshair/Crosshair.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
 
 namespace Gamebar_Crosshair
 {
@@ -20,9 +21,65 @@ namespace Gamebar_Crosshair
     /// </summary>
     public sealed partial class Crosshair : Page
     {
+        Rectangle vertical_line = new Rectangle();
+        Rectangle horizontal_line = new Rectangle();
+
+        Windows.UI.Color color = Windows.UI.Colors.Red;
+        double length = 50;
+        double thickness = 1;
+
+        private void LengthSlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            Slider slider = sender as Slider;
+            if (slider != null)
+            {
+                length = slider.Value;
+            }
+
+            vertical_line.Height = length;
+            horizontal_line.Width = length;
+        }
+
+        private void ThicknessSlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            Slider slider = sender as Slider;
+            if (slider != null)
+            {
+                thickness = slider.Value;
+            }
+
+            vertical_line.Width = thickness;
+            horizontal_line.Height = thickness;
+        }
+
         public Crosshair()
         {
             this.InitializeComponent();
+
+            Slider lengthSlider = new Slider();
+            lengthSlider.Header = "Length";
+            lengthSlider.Width = 200;
+            lengthSlider.Minimum = 1;
+            lengthSlider.Maximum = 150;
+            lengthSlider.Value = length;
+            lengthSlider.ValueChanged += LengthSlider_ValueChanged;
+            layoutRoot.Children.Add(lengthSlider);
+
+            //Slider thicknessSlider = new Slider();
+            //thicknessSlider.Header = "Thickness";
+            //thicknessSlider.Width = 200;
+            //thicknessSlider.ValueChanged += ThicknessSlider_ValueChanged;
+            //layoutRoot.Children.Add(thicknessSlider);
+
+            vertical_line.Fill = new SolidColorBrush(color);
+            vertical_line.Width = thickness;
+            vertical_line.Height = length;
+            layoutRoot.Children.Add(vertical_line);
+
+            horizontal_line.Fill = new SolidColorBrush(color);
+            horizontal_line.Width = length;
+            horizontal_line.Height = thickness;
+            layoutRoot.Children.Add(horizontal_line);
         }
     }
 }
